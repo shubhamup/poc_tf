079a2ad9f5bebbd4fdea8d993a75761737e828b1
shubhamup <shubham271@gmail.com>
2025-05-07 00:01:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
